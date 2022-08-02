d30a8be827b0f76351cf25d2289789adc1d54dbe
Athul <103881131+athulx80@users.noreply.github.com>
2022-08-02 21:28:45 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ Creator : <a href='tg://user?id={OWNER_ID}'>Me</a>\n┣⪼ Language : Python3\n┣⪼ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┣⪼ Source Code : <a href='https://github.com/LXR-YOUTUBER-AMAL/OP-Files-Store-Bot'>OP-File Store Bot</a>\n┣⪼ Channel : <a href='https://t.me/LXRYOUTUBERYT'>LXR YOUTUBER</a>\n┣⪼ YouTube Channel : <a href='https://youtube.com/channel/UCqRmM0N9Z-7bBiTqJsfFWwg'>LXR YOUTUBER</a>\n╰━━━━━━━━━━━━━━━➣</b>",
+            text = f"<b>╭━━━━━━━━━━━━━━━➣\n┣⪼ Creator : <a href='https://t.me/athulx80'>Athul</a>\n┣⪼ Language : Python3\n┣⪼ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n┣⪼ GitHub : <a href='https://github.com/athulx80'>Follow</a>\n┣⪼ Channel : <a href='https://t.me/+L8SWfrF_7m04ODZl'>Join Here</a>\n┣⪼ Sever :Heroku\n╰━━━━━━━━━━━━━━━➣</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("𝙲𝙻𝙾𝚂𝙴", callback_data = "close")
+                        InlineKeyboardButton("🔒 CLOSE", callback_data = "close")
                     ]
                 ]
             )
